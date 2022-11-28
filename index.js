201f755eb618fef458e9ee8bb44ac4bd94f5fe41
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-11-28 17:00:31 +0600

    updated booking and save payment info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -22,6 +23,40 @@ async function run() {
         const usersCollection = client.db('secondHome').collection('users');
         const bookingCollection = client.db('secondHome').collection('booking');
         const advertiseCollection = client.db('secondHome').collection('advertise');
+        const paymentsCollection = client.db('secondHome').collection('payments');
+
+        //-----make payment api
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        //add payment info
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
 
 
         app.get('/categories', async (req, res) => {
